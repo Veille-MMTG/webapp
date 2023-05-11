4e67fbd8a3e57872c4b1f3a8f5dd7608696210fe
Germain <germain.leignel@gmail.com>
2023-05-11 12:05:20 +0200

    fixed

--- app.py
+++ app.py
@@ -2,31 +2,32 @@ from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import IndexModel, TEXT, ASCENDING
-import os 
+import os
+import dotenv
+dotenv.load_dotenv()
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
 
 
-# def init_mongo_collection():
-#     # Get the database and collection
-#     db = mongo.db
-#     collection = db['articles']
+def init_mongo_collection(db_name, collection_name):
+    # Get the database and collection
+    db = mongo.cx[db_name]
+    collection = db[collection_name]
 
-#     # Create a unique index on the 'url' field
-#     url_index = IndexModel([("url", ASCENDING)], unique=True)
+    # Create a unique index on the 'url' field
+    url_index = IndexModel([("url", ASCENDING)], unique=True)
 
-#     # Create a compound text index on the 'title', 'text', 'summary', and 'keywords' fields
-#     text_index = IndexModel([("title", TEXT), ("text", TEXT), ("summary", TEXT), ("keywords", TEXT)])
+    # Create a compound text index on the 'title', 'text', 'summary', and 'keywords' fields
+    text_index = IndexModel([("title", TEXT), ("text", TEXT), ("summary", TEXT), ("keywords", TEXT)])
 
-#     # Create indexes
-#     collection.create_indexes([url_index, text_index])
+    # Create indexes
+    collection.create_indexes([url_index, text_index])
 
-#     return collection
+    return collection
 
-
-collection = mongo.db.articles
+collection = init_mongo_collection('news_db', 'articles')
 
 
 @app.route("/")
